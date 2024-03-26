4a3fd6f0f21738c643376a9645c03144f15e1335
rp6kp <rp6kp@umsystem.edu>
2024-03-26 15:06:07 -0500

    feat[backend]: add jwt token authentication with dummy users

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,14 +4,17 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RecipesModule } from './recipes/recipes.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 @Module({
     imports: [
         ConfigModule.forRoot({ isGlobal: true }),
+        AuthModule,
+        UsersModule,
         MongooseModule.forRootAsync({
             imports: [ConfigModule],
             inject: [ConfigService],
             useFactory: async (config: ConfigService) => {
-                console.log(config);
                 return {
                     uri: config.get<string>('MONGODB_CONNECTION'), // Loaded from .ENV
                 };
