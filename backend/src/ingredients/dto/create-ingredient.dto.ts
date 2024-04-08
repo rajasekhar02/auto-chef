8617d3964f1fdb94d1ad42886e4f2166824785d9
rp6kp <rp6kp@umsystem.edu>
2024-04-08 17:09:40 -0500

    feat: add validations

--- backend/src/ingredients/dto/create-ingredient.dto.ts
+++ backend/src/ingredients/dto/create-ingredient.dto.ts
@@ -1,5 +1,8 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateIngredientDto {
     readonly ingredient_id: string;
     readonly allergens: string[];
+    @IsNotEmpty()
     readonly name: string;
 }
--- backend/src/recipes/dto/create-recipe.dto.ts
+++ backend/src/recipes/dto/create-recipe.dto.ts
@@ -1,11 +1,18 @@
+import { IsNotEmpty, IsUrl, IsUUID } from 'class-validator';
 export class CreateRecipeDto {
+    @IsNotEmpty()
+    @IsUUID('4')
     readonly recipe_id: string;
+    @IsNotEmpty()
     readonly name: string;
+    @IsNotEmpty()
     readonly ingredients: string[];
     readonly cuisine: string[];
     readonly diet: string[];
     readonly meal_type: string[];
+    @IsUrl()
     readonly youtube_url: string;
+    @IsUrl()
     readonly image_url: string;
     readonly max_serving: number;
     readonly min_serving: number;
